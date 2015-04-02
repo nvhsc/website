cf6a96c10102335fb0c86d70f8a45a3fec33b248
Cody Wackerman <cwackerman@mail.csuchico.edu>
2015-04-01 22:25:43 -0700

    changing date to time in shared examples (trying to fix timezone errors)

--- spec/support/shared_examples/temporal_helper.rb
+++ spec/support/shared_examples/temporal_helper.rb
@@ -5,7 +5,7 @@ shared_examples_for 'a time-based model' do
   describe '#positive_time_flow' do
     subject(:season) { build(described_class.name.underscore.to_sym, start_at: Time.current) }
     let(:error) { 'must end after it starts' }
-    it { is_expected.to allow_value(Date.tomorrow.to_time).for(:end_at) }
-    it { is_expected.not_to allow_value(Date.yesterday.to_time).for(:end_at).with_message(error) }
+    it { is_expected.to allow_value(Time.current.tomorrow).for(:end_at) }
+    it { is_expected.not_to allow_value(Time.current.yesterday).for(:end_at).with_message(error) }
   end
 end
